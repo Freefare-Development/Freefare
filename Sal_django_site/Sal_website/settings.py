3d07471d8b7ddb49091f95ba5cf9b86c5a63caab
Justo Karell <jek124@case.edu>
2023-02-01 10:33:40 -0500

    validation only for  RecipientPostForm to start

--- Sal_django_site/Sal_website/settings.py
+++ Sal_django_site/Sal_website/settings.py
@@ -33,7 +33,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = []
-ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS"),"127.0.0.1","localhost","167.71.106.235","68.183.143.170","32.213.8.188","24.60.248.41","108.30.157.162", "192.168.1.69",] 
+ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS"),"127.0.0.1","localhost","167.71.106.235","68.183.143.170","32.213.8.188","24.60.248.41","108.30.157.162", "192.168.1.69",
+                 "74.71.89.122","24.97.185.90"] 
                          
 # ['freefoodsal.com', 'www.freefoodsal.com', '167.71.106.235', 'http://127.0.0.1:8000']
 # Application definition
--- Sal_django_site/main/forms.py
+++ Sal_django_site/main/forms.py
@@ -47,28 +47,94 @@ class RecipientPostForm(forms.ModelForm):
 
     def clean_post_title(self):
         post_title = self.cleaned_data['post_title']
-        if '@' in post_title or '-' in post_title or '|' in post_title or '&' in post_title:
+        # No Special Characters
+        if '<' in post_title or '>' in post_title or '*' in post_title or '/' in post_title or '|' in post_title or '=' in post_title:
            raise forms.ValidationError("Titles should not have special characters.")
         return post_title
 
     def clean_post_org_name(self):
         post_org_name = self.cleaned_data['post_org_name']
-        if '@' in post_org_name or '-' in post_org_name or '|' in post_org_name or '&' in post_org_name:
+        # No Special Characters
+        if '<' in post_org_name or '>' in post_org_name or '*' in post_org_name or '/' in post_org_name or '|' in post_org_name or '=' in post_org_name:
            raise forms.ValidationError("Name should not have special characters.")
         return post_org_name
+    
+    def clean_post_org_email(self):
+        post_org_email = self.cleaned_data['post_org_email']
+        # No Special Characters
+        if '<' in post_org_email or '>' in post_org_email or '*' in post_org_email or '/' in post_org_email or '|' in post_org_email or '=' in post_org_email:
+           raise forms.ValidationError("Email should not have weird characters.")
+        if '@' not in post_org_email:
+           raise forms.ValidationError("Please use a correct email address")
+        if len(post_org_email) < 6:
+            raise forms.ValidationError("Please use a correct email address")
+        return post_org_email
+
+    def clean_post_org_phone(self):
+        post_org_phone = self.cleaned_data['post_org_phone']
+        # No Special Characters
+        if '<' in post_org_phone or '>' in post_org_phone or '*' in post_org_phone or '/' in post_org_phone or '|' in post_org_phone or '=' in post_org_email:
+           raise forms.ValidationError("Phone # should not have special characters.")
+        if len(post_org_phone) < 10:
+           raise forms.ValidationError('Phone # must be at least 10 digits')
+        return post_org_phone
+    
 
     def clean_post_org_address(self):
         post_org_address = self.cleaned_data['post_org_address']
+        # No Special Characters
+        if '<' in post_org_address or '>' in post_org_address or '*' in post_org_address or '/' in post_org_address or '|' in post_org_address or '=' in post_org_address:
+            raise forms.ValidationError("Address should not have special characters.")
         post_org_address = escape(post_org_address)
+        if len(post_org_address) < 2:
+            raise forms.ValidationError("Please enter a real address")
         return post_org_address
-
+    
     def clean_post_org_country(self):
         post_org_country = self.cleaned_data['post_org_country']
+        # No Special Characters
+        if '<' in post_org_country or '>' in post_org_country or '*' in post_org_country or '/' in post_org_country or '|' in post_org_country or '=' in post_org_country:
+            raise forms.ValidationError("Country name should not have special characters.")
         post_org_country = escape(post_org_country)
-        return post_org_country    
+        if len(post_org_country) < 2:
+            raise forms.ValidationError("Please enter a real country")
+        return post_org_country 
+    
+    def clean_post_org_city(self):
+        post_org_city = self.cleaned_data['post_org_city']
+        # No Special Characters
+        if '<' in post_org_city or '>' in post_org_city or '*' in post_org_city or '/' in post_org_city or '|' in post_org_city or '=' in post_org_city:
+            raise forms.ValidationError("City name should not have special characters.")
+        post_org_city = escape(post_org_city)
+        if len(post_org_city) < 2:
+            raise forms.ValidationError("Please enter a real city")
+        return post_org_city   
+     
+    def clean_post_org_state(self):
+        post_org_state = self.cleaned_data['post_org_state']
+        # No Special Characters
+        if '<' in post_org_state or '>' in post_org_state or '*' in post_org_state or '/' in post_org_state or '|' in post_org_state or '=' in post_org_state:
+            raise forms.ValidationError("State name should not have special characters.")
+        post_org_state = escape(post_org_state)
+        if len(post_org_state) < 2:
+            raise forms.ValidationError("Please enter a real state")
+        return post_org_state   
 
+    def clean_post_org_zipcode(self):
+        post_org_zipcode = self.cleaned_data['post_org_zipcode']
+        # No Special Characters
+        if '<' in post_org_zipcode or '>' in post_org_zipcode or '*' in post_org_zipcode or '/' in post_org_zipcode or '|' in post_org_zipcode or '=' in post_org_zipcode:
+            raise forms.ValidationError("Zipcode should not have special characters.")
+        post_org_zipcode = escape(post_org_zipcode)
+        if len(post_org_zipcode) < 5:
+            raise forms.ValidationError("Please enter a real zip code")
+        return post_org_zipcode 
+      
     def clean_post_desc(self):
         post_desc = self.cleaned_data['post_desc']
+        # No Special Characters
+        if '<' in post_desc or '>' in post_desc or '*' in post_desc or '/' in post_desc or '|' in post_desc or '=' in post_desc:
+            raise forms.ValidationError("Item description should not have special characters.")
         post_desc = escape(post_desc)
         return post_desc
 
@@ -89,29 +155,51 @@ class DonorPostForm(forms.ModelForm):
     
     def clean_post_title(self):
         post_title = self.cleaned_data['post_title']
-        if '@' in post_title or '-' in post_title or '|' in post_title or '&' in post_title:
+        if '<' in post_title or '>' in post_title or '*' in post_title or '/' in post_title or '|' in post_title or '=' in post_title:
            raise forms.ValidationError("Titles should not have special characters.")
         return post_title
 
     def clean_post_org_name(self):
         post_org_name = self.cleaned_data['post_org_name']
-        if '@' in post_org_name or '-' in post_org_name or '|' in post_org_name or '&' in post_org_name:
+        if '<' in post_org_name or '>' in post_org_name or '*' in post_org_name or '/' in post_org_name or '|' in post_org_name or '=' in post_org_name:
            raise forms.ValidationError("Name should not have special characters.")
         return post_org_name
+    
+    def clean_post_org_email(self):
+        post_org_email = self.cleaned_data['post_org_email']
+        if '<' in post_org_email or '>' in post_org_email or '*' in post_org_email or '/' in post_org_email or '|' in post_org_email or '=' in post_org_email:
+           raise forms.ValidationError("Email should not have weird characters.")
+        if '@' not in post_org_email:
+           raise forms.ValidationError("Please use a correct email address")
+        return post_org_email
+    
+    def clean_post_org_phone(self):
+        post_org_phone = self.cleaned_data['post_org_phone']
+        if '<' in post_org_phone or '>' in post_org_phone or '*' in post_org_phone or '/' in post_org_phone or '|' in post_org_phone or '=' in post_org_email:
+           raise forms.ValidationError("Phone # should not have special characters.")
+        if len(post_org_phone) < 10:
+           raise forms.ValidationError('Phone # must be at least 10 digits')
+        return post_org_phone
 
     def clean_post_org_address(self):
         post_org_address = self.cleaned_data['post_org_address']
         post_org_address = escape(post_org_address)
+        if len(post_org_address) < 2:
+            raise forms.ValidationError("Please enter a real address")
         return post_org_address
 
     def clean_post_org_country(self):
         post_org_country = self.cleaned_data['post_org_country']
         post_org_country = escape(post_org_country)
+        if len(post_org_country) < 2:
+            raise forms.ValidationError("Please enter a real country")
         return post_org_country
 
     def clean_post_desc(self):
         post_desc = self.cleaned_data['post_desc']
         post_desc = escape(post_desc)
+        if '<' in post_desc or '>' in post_desc or '*' in post_desc or '/' in post_desc or '|' in post_desc or '=' in post_desc:
+           raise forms.ValidationError("Description should not have special characters.")
         return post_desc
 
 
@@ -144,11 +232,14 @@ class ProfileForm(forms.ModelForm):
     def clean_org_country(self):
         org_country = self.cleaned_data['org_country']
         org_country = escape(org_country)
+        
         return org_country
 
     def clean_org_desc(self):
         org_desc = self.cleaned_data['org_desc']
         org_desc = escape(org_desc)
+        if '<' in org_desc or '>' in org_desc or '*' in org_desc or '/' in org_desc or '|' in org_desc or '=' in org_desc:
+           raise forms.ValidationError("Description should not have special characters.")
         return org_desc
 
 class CustomUserCreationForm(UserCreationForm):
