46ddf58d0f6bce5554fa6f69f27e695fbb4988c5
Justo Karell <jek124@case.edu>
2023-05-13 12:21:38 -0400

    Turned DEBUG off

--- Sal_django_site/Sal_website/settings.py
+++ Sal_django_site/Sal_website/settings.py
@@ -29,8 +29,8 @@ load_dotenv(verbose=True, dotenv_path=os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG", "False") == "True"
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
+# DEBUG = True
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS"),"127.0.0.1","localhost","167.71.106.235","68.183.143.170","32.213.8.188","24.60.248.41","108.30.157.162", "192.168.1.69",
